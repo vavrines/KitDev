6dc0c5279cb7feb0b3e45096ad9b16f54965ceed
vavrines <tianbaixiao@gmail.com>
2021-07-23 17:05:47 +0200

    Update dsmc trainer

--- dsmc-machine/trainer.jl
+++ dsmc-machine/trainer.jl
@@ -1,25 +1,25 @@
-using Kinetic, DataFrames, Flux, LinearAlgebra, JLD2
+using Kinetic, DataFrames, Flux, LinearAlgebra, JLD2, Kinetic.KitML.DiffEqFlux
 
-isNewStart = false
+isNewStart = true#false
 cd(@__DIR__)
 
 # read dsmc data
 dfs = []
-for i = 1:50
-    df = DataFrame(idx = Int32[], u = Float32[], v = Float32[], x = Float32[], y = Float32[])
-    fname = "Track_particle/PartTrack_iter(" * string(i) * ").dat"
+for i = 1:200
+    df = DataFrame(idx = Int32[], t = Float32[], u = Float32[], v = Float32[], x = Float32[], y = Float32[])
+    fname = "Track_particle/GroupPartTrack_iter(" * string(i) * ").dat"
 
     io = open(fname)
     for line in eachline(io)
-        if length(line) == 0 || line[1] == 'P'
+        if length(line) == 0 || line[1] == 'G'
             continue
         end
 
         res = split(line, " "; keepempty = false)
         v0 = parse(Int32, res[1])
-        v1, v2, v3, v4 = parse.(Float32, res[2:end])
+        v1, v2, v3, v4, v5 = parse.(Float32, res[2:end])
 
-        push!(df, (v0, v1, v2, v3, v4))
+        push!(df, (v0, v1, v2, v3, v4, v5))
     end
     close(io)
 
@@ -27,27 +27,31 @@ for i = 1:50
 end
 
 # generate dataset
-X = zeros(Float32, 800, 1)
-Y = zeros(Float32, 800, 1)
-X = [dfs[1].u; dfs[1].v]
-Y = [dfs[2].u; dfs[2].v]
+X = [dfs[1].t; dfs[1].u; dfs[1].v]
+Y = [dfs[2].t; dfs[2].u; dfs[2].v]
 
 for i = 2:length(dfs)-1
-    _X = [dfs[i].u; dfs[i].v]
-    _Y = [dfs[i+1].u; dfs[i+1].v]
+    _X = [dfs[i].t; dfs[i].u; dfs[i].v]
+    _Y = [dfs[i+1].t; dfs[i+1].u; dfs[i+1].v]
     global X = hcat(X, _X)
     global Y = hcat(Y, _Y)
 end
 
 # neural model
 if isNewStart
-    nn = Chain(Dense(800, 800, tanh), Dense(800, 800, tanh), Dense(800, 800))
+    nn = Chain(
+        Dense(9, 36, tanh),
+        Dense(36, 72, tanh),
+        Dense(72, 72, tanh),
+        Dense(72, 36, tanh),
+        Dense(36, 9),
+    )
 else
     @load "model.jld2" nn
 end
 
 # train
-sci_train!(nn, (X, Y), ADAM(); device = cpu, batch = 7, epoch = 100000)
+sci_train!(nn, (X, Y), ADAM(); device = cpu, batch = 16, epoch = 5000)
 
 # accuracy
 function accuracy()
