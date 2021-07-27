34e6902484e49e6e29eb9d5ca5844359c18bfc7f
vavrines <tianbaixiao@gmail.com>
2021-07-27 09:13:05 +0200

    Add burgers figs

--- SFR/burgers/plot.jl
+++ SFR/burgers/plot.jl
@@ -21,17 +21,20 @@ begin
     push!(u, sol)
 end
 
-str = 12
+str = 1
+
 plot(x, u[3][:, 1], label="Lasso", lw=2, xlabel="x", ylabel="expected value")
 plot!(x, u[2][:, 1], label="Adaptive L²", lw=2, line=:dash)
-plot!(x, u0[1][:, 1], label="Standard L²", lw=2, line=:dashdot)
-scatter!(x[1:str:end], u0[4][1:str:end, 1], label="No filter", markeralpha=0.6)
+plot!(x, u0[2][:, 1], label="Optimized L²", lw=2, line=:dashdot)
+plot!(x, u0[1][:, 1], label="Standard L²", lw=2, line=:dashdotdot)
+#scatter!(x[1:str:end], u0[4][1:str:end, 1], label="No filter", markeralpha=0.6, color=4)
+plot!(x, u0[4][:, 1], label="No filter", lw=2, line=:dot, color=:gray27)
+savefig("burgers_mean.pdf")
 
 plot(x, u[3][:, 2], label="Lasso", lw=2, xlabel="x", ylabel="standard deviation")
 plot!(x, u[2][:, 2], label="Adaptive L²", lw=2, line=:dash)
-plot!(x, u0[1][:, 2], label="Standard L²", lw=2, line=:dashdot)
-scatter!(x[1:str:end], u0[4][1:str:end, 2], label="No filter", markeralpha=0.6)
-
-plot(x, u[2][:, 2], label="Adaptive L²", xlabel="x", ylabel="standard deviation")
-plot!(x, u0[2][:, 2], label="Optimizied L²", line=:dash)
-plot!(x, u0[1][:, 2], label="Standard L²", line=:dot)
+plot!(x, u0[2][:, 2], label="Optimized L²", lw=2, line=:dashdot)
+plot!(x, u0[1][:, 2], label="Standard L²", lw=2, line=:dashdotdot)
+#scatter!(x[1:str:end], u0[4][1:str:end, 2], label="No filter", markeralpha=0.6, color=4)
+plot!(x, u0[4][:, 2], label="No filter", lw=2, line=:dot, color=:gray27)
+savefig("burgers_std.pdf")
