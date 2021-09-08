51c6590868cb16f1f37e2557cc5f71c5d573b309
vavrines <tianbaixiao@gmail.com>
2021-09-08 10:00:05 +0200

    Sync

--- SFR/sod/plot.jl
+++ SFR/sod/plot.jl
@@ -2,17 +2,17 @@ using JLD2, Plots
 
 cd(@__DIR__)
 begin
-    @load "collocation.jld2" x sol1 sol2
+    @load "results/collocation.jld2" x sol1 sol2
     x0 = deepcopy(x)
     sol_collo = [sol1, sol2]
 
-    @load "l2.jld2" x sol1 sol2
+    @load "results/l2.jld2" x sol1 sol2
     sol_l2 = [sol1, sol2]
 
-    @load "l2_apt.jld2" x sol1 sol2
+    @load "results/l2_apt.jld2" x sol1 sol2
     sol_apt = [sol1, sol2]
 
-    @load "lasso.jld2" x sol1 sol2
+    @load "results/lasso.jld2" x sol1 sol2
     sol_lasso = [sol1, sol2]
 end
 
@@ -22,12 +22,12 @@ plot!(x, sol_l2[1][:, 1, 1], lw=2, label="Adaptive L²", line=:dash)
 plot!(x0, sol_collo[1][:, 1, 1], label="Reference", line=:dashdot, lw=2, color=:gray27)
 savefig("sod1_density_mean.pdf")
 
-plot(x, sol_lasso[1][:, 2, 1], lw=2, label="Lasso", xlabel="x", ylabel="velocity")
+plot(x, sol_lasso[1][:, 2, 1], lw=2, label="Lasso", xlabel="x", ylabel="velocity", legend=:topleft)
 plot!(x, sol_l2[1][:, 2, 1], lw=2, label="Adaptive L²", line=:dash)
 plot!(x0, sol_collo[1][:, 2, 1], label="Reference", line=:dashdot, lw=2, color=:gray27)
 savefig("sod1_velocity_mean.pdf")
 
-plot(x, sol_lasso[1][:, 3, 1], lw=2, label="Lasso", xlabel="x", ylabel="temperature")
+plot(x, sol_lasso[1][:, 3, 1], lw=2, label="Lasso", xlabel="x", ylabel="temperature", legend=:topleft)
 plot!(x, sol_l2[1][:, 3, 1], lw=2, label="Adaptive L²", line=:dash)
 plot!(x0, sol_collo[1][:, 3, 1], label="Reference", line=:dashdot, lw=2, color=:gray27)
 savefig("sod1_temperature_mean.pdf")
@@ -42,7 +42,7 @@ plot!(x, sol_l2[1][:, 2, 2], lw=2, label="Adaptive L²", line=:dash)
 plot!(x0, sol_collo[1][:, 2, 2], label="Reference", line=:dashdot, lw=2, color=:gray27)
 savefig("sod1_velocity_std.pdf")
 
-plot(x, sol_lasso[1][:, 3, 2], lw=2, label="Lasso", xlabel="x", ylabel="temperature")
+plot(x, sol_lasso[1][:, 3, 2], lw=2, label="Lasso", xlabel="x", ylabel="temperature", legend=:topleft)
 plot!(x, sol_l2[1][:, 3, 2], lw=2, label="Adaptive L²", line=:dash)
 plot!(x0, sol_collo[1][:, 3, 2], label="Reference", line=:dashdot, lw=2, color=:gray27)
 savefig("sod1_temperature_std.pdf")
@@ -58,7 +58,7 @@ plot!(x, sol_l2[2][:, 2, 1], lw=2, label="Adaptive L²", line=:dash)
 plot!(x0, sol_collo[2][:, 2, 1], label="Reference", line=:dashdot, lw=2, color=:gray27)
 savefig("sod2_velocity_mean.pdf")
 
-plot(x, sol_lasso[2][:, 3, 1], lw=2, label="Lasso", xlabel="x", ylabel="temperature")
+plot(x, sol_lasso[2][:, 3, 1], lw=2, label="Lasso", xlabel="x", ylabel="temperature", legend=:topleft)
 plot!(x, sol_l2[2][:, 3, 1], lw=2, label="Adaptive L²", line=:dash)
 plot!(x0, sol_collo[2][:, 3, 1], label="Reference", line=:dashdot, lw=2, color=:gray27)
 savefig("sod2_temperature_mean.pdf")
